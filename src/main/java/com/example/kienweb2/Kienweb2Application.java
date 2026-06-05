a872d70b5f93d0d1f737f56c7191cbfb02c2c5a9
Kien637 <regviothanlong@gmail.com>
2026-06-05 08:44:03 +0700

    Initial commit

---
+++ src/main/java/com/example/kienweb2/Kienweb2Application.java
@@ -0,0 +1,13 @@
+package com.example.kienweb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kienweb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kienweb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kienweb2/Kienweb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kienweb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kienweb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
